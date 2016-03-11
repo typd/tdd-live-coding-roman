4d8959ae6444a6acad32b06dc95882472d964150
typd <othertang@gmail.com>
2016-03-11 11:37:47 +0800

    update

--- roman-code/src/main/java/typd/Converter.java
+++ roman-code/src/main/java/typd/Converter.java
@@ -2,18 +2,29 @@ package typd;
 
 public class Converter {
 
-    public String convertRomanNumber(int value){
-        if (value == 1)
-            return "I";
-        if (value == 2)
-            return "II";
-        if (value == 3)
-            return "III";
-        if (value == 4)
-            return "IV";
-        if (value == 5)
-            return "V";
-        return null;
+    public String convertRomanNumber(int value) {
+        if (value <= 0)
+            throw new RuntimeException("value should > 0, but now it's: " + value);
+        String result = "";
+
+        boolean hasFirstI = false;
+        if ((value - 3) % 5 == 1) {
+            result += "I";
+            hasFirstI = true;
+        }
+
+        if (value >= 9)
+            result += "X";
+        else if (value >= 4)
+            result += "V";
+
+        if (!hasFirstI) {
+            int lastIs = (value % 5);
+            for (int i = 1; i <= lastIs; i++) {
+                result += "I";
+            }
+        }
+        return result;
     }
 
 }
--- roman-code/src/test/java/typd/ConverterTest.java
+++ roman-code/src/test/java/typd/ConverterTest.java
@@ -5,6 +5,19 @@ import junit.framework.TestCase;
 public class ConverterTest extends TestCase {
     private Converter converter = new Converter();
 
+    public void testIllegal() {
+        try {
+            converter.convertRomanNumber(-1);
+            fail();
+        } catch (Exception e) {
+        }
+        try {
+            converter.convertRomanNumber(0);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
     public void test1() {
         assertSame("I", 1);
     }
@@ -17,21 +30,35 @@ public class ConverterTest extends TestCase {
         assertSame("III", 3);
     }
 
-    public void test4() {
-        assertSame("IV", 4);
-    }
-
     public void test5() {
         assertSame("V", 5);
     }
 
-    public void test6to8() {
+    public void test6() {
         assertSame("VI", 6);
+    }
+
+    public void test7() {
         assertSame("VII", 7);
+    }
+
+    public void test8() {
         assertSame("VIII", 8);
     }
 
+    public void test4and9() {
+        assertSame("IV", 4);
+        assertSame("IX", 9);
+    }
+
+    public void test10to13() {
+        assertSame("X", 10);
+        assertSame("XI", 11);
+        assertSame("XII", 12);
+        assertSame("XIII", 13);
+    }
+
     private void assertSame(String expect, int roman) {
-        assertSame(expect, converter.convertRomanNumber(roman));
+        assertEquals(expect, converter.convertRomanNumber(roman));
     }
 }
